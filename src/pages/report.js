6ae5fee79b7acadd2557052764a4f7305fc2758d
Ivo Gregurec <ivo.gregurec@gmail.com>
2021-07-27 23:03:57 +0200

    report page

---
+++ src/pages/report.js
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+
+const reportPage = () => (
+    <Layout>
+        <Seo title="Report" />
+        <h1>Prijavite:</h1>
+        <ul className={"ml-0"}>
+            <li>
+                <Link to="/">Smrad</Link>
+            </li>
+            <li>
+                <Link to="/">Buku</Link>
+            </li>
+            <li>
+                <Link to="/">Otpad</Link>
+            </li>
+            <li>
+                <Link to="/">Zagađenje</Link>
+            </li>
+        </ul>
+    </Layout>
+)
+
+export default reportPage
