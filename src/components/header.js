34af179818721b54cbb4bcfde1f2c0389cf88e18
Ivo Gregurec <ivo.gregurec@gmail.com>
2021-07-29 13:51:11 +0200

    menu item in header

--- src/components/header.js
+++ src/components/header.js
@@ -10,10 +10,15 @@ const Header = ({ siteTitle }) => (
         <div
             className={"py-0"}
         >
+            <button className={"inline-block mr-2"}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="white">
+                    <path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd" />
+                </svg>
+            </button>
 
             <Link
               to="/"
-              className={"text-white"}
+              className={"text-white inline-block"}
             >
               {siteTitle}
             </Link>
