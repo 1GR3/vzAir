f78fac937e351484fac872d65e62d3a5bad9dd62
Ivo Gregurec <ivo.gregurec@gmail.com>
2021-08-05 16:14:31 +0200

    gatsby-plugin-modal-routing with options

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,18 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-modal-routing`,
+      options: {
+        // A selector to set react-modal's app root to, default is `#___gatsby`
+        // See http://reactcommunity.org/react-modal/accessibility/#app-element
+        appElement: '#___gatsby',
+
+        // Object of props that will be passed to the react-modal container
+        // See http://reactcommunity.org/react-modal/#usage
+        modalProps: { },
+      }
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
