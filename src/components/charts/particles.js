4fa53f3cca300d54aec87abd06f024d28c2a500c
Ivo Gregurec <ivo.gregurec@gmail.com>
2021-08-12 17:27:22 +0200

    particles chart

--- src/components/charts/particles.js
+++ src/components/charts/particles.js
@@ -1,27 +1,38 @@
 import React from 'react';
-import ReactECharts from 'echarts-for-react';
+import Chart from "react-google-charts";
 
-var myChart = echarts.init(document.getElementById('main'));
+function Particles () {
 
-// specify chart configuration item and data
-var option = {
-    title: {
-        text: 'ECharts entry example'
-    },
-    tooltip: {},
-    legend: {
-        data:['Sales']
-    },
-    xAxis: {
-        data: ["shirt","cardign","chiffon shirt","pants","heels","socks"]
-    },
-    yAxis: {},
-    series: [{
-        name: 'Sales',
-        type: 'bar',
-        data: [5, 20, 36, 10, 10, 20]
-    }]
-};
+    return (
+        <Chart
+            width={'100%'}
+            height={'300px'}
+            chartType="AreaChart"
+            loader={<div>Loading Chart</div>}
+            data={[
+                ['Date', '0.3-2.5', '2.5-5','5+'],
+                ['2013', 22, 19, 18],
+                ['2014', 34, 27, 25],
+                ['2015', 26, 20, 11],
+                ['2016', 23, 20, 14],
+                ['2017', 21, 16, 16],
+            ]}
+            options={{
+                title: 'Nano Čestice',
+                curveType: 'function',
+                legend: { position: 'bottom' },
+                // hAxis: { title: 'Year', titleTextStyle: { color: '#333' } },
+                vAxis: { minValue: 0 },
+                // For the legend to fit, we make the chart area smaller
+                chartArea: { width: '90%', height: '70%' },
+                colors: ['#0ec29e', '#30cec8', '#6de3f3', '#aae2fc', '#f6c7b6'],
+                backgroundColor: { fill:'transparent' },
+                lineWidth: 2
+            }}
+            // For tests
+            rootProps={{ 'data-testid': '1' }}
+        />
+    )
+}
 
-// use configuration item and data specified to show chart
-myChart.setOption(option);
+export default Particles
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import Chart from "react-google-charts";
+import Particles from "../components/charts/particles"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -8,9 +10,16 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
       <div className={"flex sm:inline-block md:block"}>
-          <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">temperatura/tlak/relativna vlaga</div>
-          <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">Nano čestice 0.3-2.5/2.5-5/5+</div>
-          <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">amonijak/okidacijski/redukcijski</div>
+          <div className="flex-1 text-gray-700 text-center bg-gradient-to-br from-gray-400 to-gray-500 px-4 py-2 m-2">
+              temperatura/tlak/relativna vlaga
+          </div>
+          <div className="flex-1 text-gray-700 text-center bg-gradient-to-br from-gray-400 to-gray-500 p-1 m-2">
+
+              <Particles/>
+          </div>
+          <div className="flex-1 text-gray-700 text-center bg-gradient-to-br from-gray-400 to-gray-500 py-2 m-2">
+              amonijak/okidacijski/redukcijski
+          </div>
       </div>
       <Seo title="Home" />
       <h1>VŽ Air</h1>
