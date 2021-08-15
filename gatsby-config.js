1c426278900a6f1c2914506ea8c34579affa04d0
Ivo Gregurec <ivo.gregurec@gmail.com>
2021-08-15 21:37:31 +0200

    gatsby-plugin-styled-components

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     'gatsby-plugin-postcss',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
